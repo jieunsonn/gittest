faaecb0e46e32abc49a82b2cb66622ec0a0fbb2e
archi <archi@DESKTOP-6FC56U4>
2023-10-17 16:51:19 +0900

    bs_dev브랜치에서 main 수정

--- gittest/src/com/gittest/run/Main.java
+++ gittest/src/com/gittest/run/Main.java
@@ -1,8 +1,11 @@
 package com.gittest.run;
 
+import java.net.MulticastSocket;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("안녕 git 반가워");
+		System.out.println("난 dev 브랜치 우하하하ㅏ");
 
 	}
 
