afe32725c54db9295b1306b140ac6d2c42a04aca
archi <archi@DESKTOP-6FC56U4>
2023-10-18 09:22:09 +0900

    bs main 메소드 수정22

--- gittest/src/com/gittest/run/Main.java
+++ gittest/src/com/gittest/run/Main.java
@@ -1,9 +1,11 @@
 package com.gittest.run;
 
+import com.gittest.controller.GitController;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new GitController().test();
 
 	}
 
